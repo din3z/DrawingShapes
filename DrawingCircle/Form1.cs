eb1b9cef37c1eeb709ce745cc55c4ad0eb098202
Sady <khside17@gmail.com>
2021-01-02 17:51:09 +0500

    только создание окружностей

---
+++ DrawingCircle/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Numerics;
+
+namespace DrawingCircle
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<Circle> list = new List<Circle>();
+        Storage<Circle> libr = new Storage<Circle>(50);
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            for (int i = 0; i < libr._size; i++)
+                if (libr.get_Shape(i) != null)
+                    libr.get_Shape(i).Draw(e);
+            
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Circle circle = new Circle();
+            libr.add(new Circle(e.X - 25, e.Y - 25));
+            this.Invalidate();
+            //if ((e.Button == MouseButtons.Right) && (circle.isCircle(e)==true))
+            
+
+        }
+        public class Circle
+        {
+            public int _radius=25;
+            public int _x;
+            public int _y;
+            public Circle()
+            {
+            }
+            public Circle( int x, int y)
+            {
+                _x = x;
+                _y = y;
+            }
+            public int get_x()
+            {
+                return _x;
+            }
+            public int get_y()
+            {
+                return _y;
+            }
+            public void Draw(PaintEventArgs e)
+            {
+                Pen pen = new Pen(Color.Green);
+                e.Graphics.DrawEllipse(pen, _x, _y, _radius * 2, _radius * 2);
+            }
+            public bool isCircle(MouseEventArgs e)
+            {
+                return (((_x - e.X) * (_x - e.X) + (_y - e.Y) * (_y - e.Y) <= _radius * _radius));
+            }
+        }
+    }
+}
---
+++ DrawingCircle/Storage.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawingCircle
+{
+    public class Storage<Shape>
+    {
+        private int _n;//количество занятых ячеек
+        public Shape[] masShape;
+        public int _size;//размер
+        public Storage(int size)
+        {
+            _n = 0;
+            _size = size;
+            masShape = new Shape[_size];
+            for (int i = 0; i < _size; i++)
+            {
+                masShape[i] = default(Shape);
+            }
+        }
+
+        public void add(Shape b)
+        {
+            if (_n >= _size)
+                change_size(_n + 1);
+
+            if (_n < _size && masShape[_n] == null)
+            {
+                masShape[_n] = b;
+                _n++;
+            }
+        }
+
+       public Shape get_Shape(int i)
+        {
+            if ((i > -1) && (i < _size))
+            {
+                return masShape[i];
+            }
+            else
+            {
+                return default;
+            }
+        }
+
+        void change_size(int size)
+        {
+            {
+                Shape[] masShape1 = new Shape[_size];
+                int newsize = _size < size ? _size : size;
+                for (int i = 0; i < size; i++)
+                {
+                    masShape1[i] = masShape[i];
+                }
+                masShape = masShape1;
+                _size = newsize;
+            }
+        }
+
+        void delete_Shape(int i)
+        {
+            if (masShape[i] != null)
+            {
+                masShape[i] = default;
+                _n--;
+            }
+        }
+        public int get_size()
+        {
+            return _size;
+        }
+        public int get_num()
+        {
+            return _n;
+        }
+        Shape next_Shape(int i)
+        {
+            i++;
+            return masShape[i];
+        }
+        ~Storage()
+        {
+            for (int i = 0; i < _size; i++)
+            {
+                masShape[i] = default(Shape);
+            }
+        }
+    }
+}
